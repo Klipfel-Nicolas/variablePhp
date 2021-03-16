526f77f2c0c930994c5f1e6fcc0ad3c71b1e4c85
Klipfel-Nicolas <klipfelnicolas86@gmail.com>
2021-03-16 08:46:57 +0100

    quete variables php

---
+++ quetes/Php/variables/variables.php
@@ -0,0 +1,22 @@
+<?php
+
+$movieTitle = (string) "Indiana Jones et la dernière croisade ";
+echo gettype($movieTitle), ': ';
+echo $movieTitle, '&nbsp;&nbsp;';
+
+
+
+$saw = (bool) true;
+echo gettype($saw), ': ';
+echo $saw, '&nbsp;&nbsp;';
+
+
+
+$release = (int) 1989;
+echo gettype($release), ': ';
+echo $release, '&nbsp;&nbsp;';
+
+
+$rate = (float) 8.2;
+echo gettype($rate), ': ';
+echo $rate ;
